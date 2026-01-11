171645b6a1ec093c41f416adcf29fbd3211a7fc7
Anuj Kushwaha <anujkushwaha9926@gmail.com>
2026-01-11 10:50:35 +0530

    Add files via upload

---
+++ While Loop Problem_5.py
@@ -0,0 +1,10 @@
+"""
+W A P to calculate the product of number from 1 to 5
+Expected Output:120
+"""
+product = 1
+number = 1
+while number <= 5:
+    product *= number
+    number += 1
+print(product) 
