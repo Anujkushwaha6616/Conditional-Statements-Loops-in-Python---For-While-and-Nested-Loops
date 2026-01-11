9f2fb1e952ca35487c4fe51b5ace1aa955d196da
Anuj Kushwaha <anujkushwaha9926@gmail.com>
2026-01-11 10:47:46 +0530

    Add files via upload

---
+++ While Loop problem_1.py
@@ -0,0 +1,10 @@
+'''
+W A P to print number from 10 to 15 using while loop
+Expected Output: 10,11,12,13,14,15 
+'''
+number = 10
+
+while number <= 15:
+    print(number, end=" ")
+    number+=1
+    
