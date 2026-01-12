576594dcf2ce3cd94f83cd18300f30e472d14a9b
Anuj Kushwaha <anujkushwaha9926@gmail.com>
2026-01-12 15:24:42 +0530

    Refactor input prompts in While Loop problem_8.py
    Updated input prompts for clarity.

--- While Loop problem_8.py
+++ While Loop problem_8.py
@@ -1,9 +1,11 @@
 '''
+Keep reading number from the user and calculate the sum until the input is 0.
 '''
 total = 0
-number = int(input("Enter a number (enter 0 to stop ):"))
+number = int(input("Enter a number (Enter 0 for sum ):"))
 while number != 0:
      total += number
-     number = int(input("Enter a number(ENter 0 to stop):"))
+     number = int(input("Enter a number(Enter 0 for sum ):"))
          
 print("Total sum:",total)
+
