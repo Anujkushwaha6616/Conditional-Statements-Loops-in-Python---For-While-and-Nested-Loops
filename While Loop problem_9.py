3b273c8aab0a5c740219b84d6bafcf9ea2b5c193
Anuj Kushwaha <anujkushwaha9926@gmail.com>
2026-01-12 15:22:34 +0530

    Update While Loop problem_9.py

--- While Loop problem_9.py
+++ While Loop problem_9.py
@@ -1,8 +1,9 @@
 '''
-
+W A P goes through users input and stop as soon as it find the first even number .
 '''
 while True:
     num = int(input("Enter a number"))
     if num % 2 == 0:
         print("First Even number found :", num)
         break
+
