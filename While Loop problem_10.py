230a7b822a890b74df157aa7303943a65e59d015
Anuj Kushwaha <anujkushwaha9926@gmail.com>
2026-01-13 20:31:58 +0530

    Add files via upload

---
+++ While Loop problem_10.py
@@ -0,0 +1,12 @@
+'''
+Print the first five multiples of 3
+Expected output: 3,6,9,12,15
+'''
+multiple = 3
+count = 1
+
+while count <= 5:
+    print(multiple, end=" ")
+    multiple += 3
+    count += 1
+    
