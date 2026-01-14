a262c3d387ff99f7278a3c2a26c3ae9ff3f46dbf
Anuj Kushwaha <anujkushwaha9926@gmail.com>
2026-01-14 20:16:01 +0530

    Refine comments and expected output in the script
    Updated comments and expected output in While Loop problem_9.py.

--- While Loop problem_9.py
+++ While Loop problem_9.py
@@ -1,9 +1,11 @@
 '''
 W A P goes through users input and stop as soon as it find the first even number .
+Expected output:first even number found:2
 '''
 while True:
     num = int(input("Enter a number"))
     if num % 2 == 0:
         print("First Even number found :", num)
         break
+
 
