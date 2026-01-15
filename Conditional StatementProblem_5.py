7f829e8524e4d544488eb931d89aca53fda5103b
Anuj Kushwaha <anujkushwaha9926@gmail.com>
2026-01-15 19:58:17 +0530

    Add files via upload

---
+++ Conditional StatementProblem_5.py
@@ -0,0 +1,8 @@
+'''
+The break statement in Python brings control out of the loop.
+'''
+for letter in 'geeksforgeeks':
+    if letter == 'e' or letter == 's':
+        break
+
+print('Current Letter :', letter)
