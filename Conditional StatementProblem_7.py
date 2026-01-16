47f683ceb2ec4043f6eb1130699ce30e08a52aab
Anuj Kushwaha <anujkushwaha9926@gmail.com>
2026-01-16 20:22:42 +0530

    Add files via upload

---
+++ Conditional StatementProblem_7.py
@@ -0,0 +1,18 @@
+'''
+Given an array, count how many elements are strictly greater than every element before them.
+
+Input: [3, 4, 2, 7, 5, 9]
+Output: 4
+'''
+def count_greater_previous(arr):
+    count = 0
+    current_max = float('-inf')
+
+    for num in arr:
+        if num > current_max:
+            count += 1
+            current_max = num
+
+    return count
+
+print(count_greater_previous([3, 4, 2, 7, 5, 9]))
