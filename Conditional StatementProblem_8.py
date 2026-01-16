0dad01b533fed00dd0b65365bab23d42966cf4ae
Anuj Kushwaha <anujkushwaha9926@gmail.com>
2026-01-16 20:23:06 +0530

    Add files via upload

---
+++ Conditional StatementProblem_8.py
@@ -0,0 +1,19 @@
+'''
+Given an array, print how many times each number appears.
+
+Example
+
+Input: [1, 2, 2, 3, 1, 4]
+Output: {1: 2, 2: 2, 3: 1, 4: 1}
+'''
+def frequency(arr):
+    freq = {}
+    for num in arr:
+        if num in freq:
+            freq[num] += 1
+        else:
+            freq[num] = 1
+    return freq
+
+print(frequency([1, 2, 2, 3, 1, 4]))
+
