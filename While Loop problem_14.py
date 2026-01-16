4e62ddca13547322d1e66f46cc6cd72f23a2b009
Anuj Kushwaha <anujkushwaha9926@gmail.com>
2026-01-16 20:24:20 +0530

    Add files via upload

---
+++ While Loop problem_14.py
@@ -0,0 +1,20 @@
+'''
+Problem: Find the Sum of Digits Until the Number Becomes Single Digit
+
+(Repeated sum → digital root style)
+
+Example
+
+Input: 987
+Output: 6
+'''
+def digit_sum_until_one(n):
+    while n > 9:
+        temp = 0
+        while n > 0:
+            temp += n % 10
+            n //= 10
+        n = temp
+    return n
+
+print(digit_sum_until_one(987))
