7d3f4feb84f48b7375df9a5b77bab873d15fc182
Anuj Kushwaha <anujkushwaha9926@gmail.com>
2026-01-17 19:39:05 +0530

    Add files via upload

---
+++ Conditional StatementProblem_10.py
@@ -0,0 +1,18 @@
+def count_subarrays_sum_k(arr, k):
+    count = 0
+
+    for start in range(len(arr)):
+        current_sum = 0
+
+        for end in range(start, len(arr)):
+            current_sum += arr[end]
+
+            if current_sum == k:
+                count += 1
+
+    return count
+
+# Example
+arr = [3, 4, 7, 2, -3, 1, 4, 2]
+K = 7
+print(count_subarrays_sum_k(arr, K))
