10e952265a560edad16aa45ccc5011ffb08015d0
Anuj Kushwaha <anujkushwaha9926@gmail.com>
2026-01-19 21:05:29 +0530

    Add files via upload

---
+++ Conditional StatementProblem_12.py
@@ -0,0 +1,20 @@
+'''
+
+'''
+def count_quadruplets(arr, K):
+    n = len(arr)
+    count = 0
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            for m in range(j + 1, n):
+                for n2 in range(m + 1, n):
+                    if arr[i] + arr[j] + arr[m] + arr[n2] == K:
+                        count += 1
+
+    return count
+
+# Example
+arr = [1, 0, -1, 0, -2, 2]
+K = 0
+print(count_quadruplets(arr, K))  # Output: 3
