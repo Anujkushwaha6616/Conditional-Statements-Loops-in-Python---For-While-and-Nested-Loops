50935b43a88830c19ef33e1885393c337bca0b3b
Anuj Kushwaha <anujkushwaha9926@gmail.com>
2026-01-28 17:20:00 +0530

    Refactor steps_to_one function with problem statement
    Added problem statement and example for clarity.

--- Conditional StatementProblem_15.py
+++ Conditional StatementProblem_15.py
@@ -1,3 +1,19 @@
+'''
+Problem
+
+Given a number, perform steps:
+
+If number is even → divide by 2
+
+If number is odd → subtract 1
+Count how many steps are required to reach 1.
+
+Example
+Input:  n = 14
+Output: 5
+Explanation:
+14→7→6→3→2→1 → total 5 steps
+'''
 def steps_to_one(n):
     steps = 0
 
@@ -12,3 +28,4 @@ def steps_to_one(n):
 
 
 print(steps_to_one(14))
+
