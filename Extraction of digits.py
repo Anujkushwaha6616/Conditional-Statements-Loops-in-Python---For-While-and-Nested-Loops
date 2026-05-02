c5a35b0c85278b29396637ae0e729623fbb5cfcd
Anuj Kushwaha <anujkushwaha9926@gmail.com>
2026-05-02 20:24:20 +0530

    Refactor comments in digit extraction script
    Updated comments for clarity and consistency.

--- Extraction of digits.py
+++ Extraction of digits.py
@@ -1,8 +1,8 @@
-#Extraction of digits using loops
+#Extraction of digits which is provided by user.
 n = int(input("enter number : "))
 num = n
-#loop
+#While loop
 while num > 0:
     last_digit = num%10
-    print(last_digit)
     num = num//10
+    print(last_digit)
