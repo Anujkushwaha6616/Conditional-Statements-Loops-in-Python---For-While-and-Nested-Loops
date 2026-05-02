0c7942526ab81cab8a36945fdef78e1eb8ac0afd
Anuj Kushwaha <anujkushwaha9926@gmail.com>
2026-05-02 20:17:19 +0530

    Add files via upload

---
+++ Count digits.py
@@ -0,0 +1,9 @@
+#count the digit of the given number
+n = input("Enter to count value of digit:")
+count = 0
+num =int(n)
+while num > 0:
+    #last_digit = n%10
+    num = num//10
+    count+=1
+print(count) 
