3ac87df52f699b47c0141900a108eeb338ad87df
Anuj Kushwaha <anujkushwaha9926@gmail.com>
2026-05-05 19:20:24 +0530

    Add files via upload

---
+++ Armstrong number 4.py
@@ -0,0 +1,10 @@
+n = int(input("enter number to check armstrong or not:"))
+num = n
+result = 0 
+powers = len(str(n))
+
+while num>0 :
+      ld = num%10
+      result = result + ld**powers
+      num = num//10 
+print (result == n)    
